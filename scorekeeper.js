dbd904f007fcf14a22ea7db74a581ec3c1171526
ritikasource <ritika99.work@gmail.com>
2019-11-29 23:31:13 +0530

    Added the updating score functionality in js file.

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,12 +1,39 @@
 var p1button= document.querySelector(".p1");
 var p2button= document.querySelector(".p2");
 var p3button= document.querySelector(".p3");
+var disp1= document.querySelector("#p1Display");
+var disp2=document.querySelector("#p2Display")
+
+var gameover=false;
+var wingamescore=5;
 
 var h1=document.querySelector("h1");
-var pScore=0;
+var Score=0;
+
 p1button.addEventListener("click",function()
 {
-	pScore++;
-	h1.textContent(pScore);
+if(!gameover)
+{
+   Score++;
+   
+		if (Score===wingamescore)
+	       gameover=true;
 
+ disp1.textContent=Score;
+}
+
+});
+
+var Score2=0;
+p2button.addEventListener("click",function()
+{
+	if(!gameover)
+	{
+		Score2++;
+
+		if (Score2===wingamescore)
+	     gameover=true;
+	 
+	 	   disp2.textContent=Score;
+	 	}
 });
