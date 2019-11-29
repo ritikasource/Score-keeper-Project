af09d48fe31925245cced377a13f7003fc2e4aed
ritikasource <ritika99.work@gmail.com>
2019-11-29 18:50:47 +0530

    Added the functionality to the button.

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,1 +1,12 @@
-alert("connected!")
+var p1button= document.querySelector(".p1");
+var p2button= document.querySelector(".p2");
+var p3button= document.querySelector(".p3");
+
+var h1=document.querySelector("h1");
+var pScore=0;
+p1button.addEventListener("click",function()
+{
+	pScore++;
+	h1.textContent(pScore);
+
+});
